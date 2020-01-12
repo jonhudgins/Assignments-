84bd999f8473e87b64ed9a126582d2d786b6a30a
Jonathan Hudgins <Jhudgins@hks.harvard.edu>
2020-01-11 20:58:23 -0500

    add functions to calculator

---
+++ assignments/calculator.py
@@ -0,0 +1,15 @@
+# Multiply 
+def multiply(a,b):
+    return a * b
+
+# Add 
+def Add(a,b):
+    return a + b
+
+# Divide 
+def Divide(a,b):
+    return a/b
+
+# Subtract 
+def Subtract(a,b):
+    return a-b
